79a659ed9630feea4114f49fb514816394721005
Fady <fedo.zaher@gmail.com>
2025-02-22 19:21:16 +0200

    enhance

--- lib/models/massage_model.dart
+++ lib/models/massage_model.dart
@@ -1,26 +1,28 @@
-class MassageModel
-{
+class MassageModel {
 
-  dynamic text,dateTime,senderId,receiverId,image,createdAt;
-  MassageModel({required this.createdAt,required this.text,required this.dateTime,required this.receiverId,this.senderId,required this.image});
+  dynamic text, dateTime, senderId, receiverId, image, createdAt;
 
-  MassageModel.fromJson(Map<String,dynamic>?json)
+  MassageModel(
+      {required this.createdAt, required this.text, required this.dateTime, required this.receiverId, this.senderId, required this.image,});
+
+  MassageModel.fromJson(Map<String, dynamic>?json)
   {
-    senderId=json!['senderId'];
-    receiverId=json['receiverId'];
-    text=json['text'];
-    dateTime=json['dateTime'];
-    image=json['image'];
-    createdAt=json['createdAt'];
+    senderId = json!['senderId'];
+    receiverId = json['receiverId'];
+    text = json['text'];
+    dateTime = json['dateTime'];
+    image = json['image'];
+    createdAt = json['createdAt'];
   }
-  Map<String,dynamic> toMap(){
-    return{
-      'senderId':senderId,
-      'text':text,
-      'receiverId':receiverId,
-      'dateTime':dateTime,
-      'image':image,
-      'createdAt':createdAt
+
+  Map<String, dynamic> toMap() {
+    return {
+      'senderId': senderId,
+      'text': text,
+      'receiverId': receiverId,
+      'dateTime': dateTime,
+      'image': image,
+      'createdAt': createdAt
     };
   }
 }
